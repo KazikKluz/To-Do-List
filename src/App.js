6ed2a1a4e9adfe5dfc3f2835c2da36d296682583
Kazik Kluz <kazik.kluz@gmail.com>
2019-11-11 21:08:12 +0000

    final

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { TodoBanner } from "./TodoBanner";
 import { TodoCreator } from "./TodoCreator";
 import { TodoRow } from "./TodoRow";
+import {VisibilityControl} from "./VisibilityControl";
 
 export default class App extends Component {
   constructor(props) {
@@ -13,8 +14,8 @@ export default class App extends Component {
         { action: "Get Shoes", done: false },
         { action: "Collect Tickets", done: true },
         { action: "Call Joe", done: false }
-      ]
-      //newItemText: ""
+      ],
+      showCompleted: true
     };
   }
 
@@ -26,7 +27,7 @@ export default class App extends Component {
     if (!this.state.todoItems.find(item => item.action === task)) {
       this.setState({
         todoItems: [...this.state.todoItems, { action: task, done: false }]
-      });
+      }, ()=>localStorage.setItem("todos", JSON.stringify(this.state)));
     }
   };
 
@@ -37,10 +38,26 @@ export default class App extends Component {
       )
     });
 
-  todoTableRows = () =>
-    this.state.todoItems.map(item => (
-      <TodoRow key={item.action} item={item} callback={this.toggleTodo} />
-    ));
+  todoTableRows = (doneValue) =>
+    this.state.todoItems.filter(item => item.done === doneValue).map( item =>
+      <TodoRow key={item.action} item={item} 
+        callback={this.toggleTodo} />
+    );
+
+  componentDidMount = () =>{
+    let data = localStorage.getItem("todos");
+    this.setState(data != null
+      ? JSON.parse(data)
+      : {
+        userName: "Adam",
+        todoItems: [{action: "Buy Flowers", done: false},
+                    {action: "Get Shoes", done: false},
+                    {action: "Collect Tickets", done: true},
+                    {action: "Call Joe", done: false}],
+        showCompleted: true
+      }
+      );
+  }
 
   render = () => (
     <div>
@@ -54,8 +71,23 @@ export default class App extends Component {
               <th>Done</th>
             </tr>
           </thead>
-          <tbody>{this.todoTableRows()}</tbody>
+          <tbody>{this.todoTableRows(false)}</tbody>
         </table>
+        <div className="bg-secondary text-white text-center p-2">
+          <VisibilityControl description="Completed Tasks"
+            isChecked={this.state.showCompleted}
+            callback={ (checked) =>
+              this.setState({showCompleted: checked})} />
+        </div>
+
+        {this.state.showCompleted && 
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr><th>Description</th><th>Done</th></tr>
+            </thead>
+            <tbody>{this.todoTableRows(true)}</tbody>
+          </table>
+        }
       </div>
     </div>
   );
